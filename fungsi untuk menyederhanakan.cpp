f25fe14df31f5eb36a95e651c7ac22de984ccfd3
sitisofiyah <jarakostacity@gmail.com>
2019-01-04 17:49:35 +0700

    Add files via upload

---
+++ fungsi untuk menyederhanakan.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void pecahan();
+main()
+{
+    pecahan();
+}
+
+void pecahan()
+{
+    int a,b,l,t;
+    printf("masukkan pembilang : ");
+    scanf("%d",&a);
+    printf("masukkan penyebut : ");
+    scanf("%d",&b);
+    t = a/b;
+    l = a%b;
+    if (a%b == 0)
+        printf("bilangan (%d/%d) \ndi sederhanakan menjadi %d",a,b,t);
+    else if (a%b != 0)
+        printf("bilangan (%d/%d) \ndi sederhanakan menjadi (%d%d/%d)",a,b,t,l,b);
+}
